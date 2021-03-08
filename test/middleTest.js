c0b4aec7ac83d29ffc86d0dc84f28424c1d7d852
Adam Hart <adamhayhart@gmail.com>
2021-03-08 14:10:29 -0800

    refactoring tailTest and MiddleTest to use mocha chai assert functions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,18 +1,48 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
-
-
-middle([])
-middle([1]) // => []
-middle([1, 2]) // => []
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+const assert = require('chai').assert;
 
 const numArr = [1, 7, 12, 3, 1000];
-middle(numArr);
-assertArraysEqual(numArr, ([1, 7, 12, 3, 1000]));
-assertArraysEqual((middle(numArr)), [12]);
+
+describe("#middle", () => {
+
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("returns [12] for numArr", () => {
+    assert.deepEqual(middle(numArr), [12]);
+  });
+
+});
+
+// middle([])
+// middle([1]) // => []
+// middle([1, 2]) // => []
+// // For arrays with odd number of elements, an array containing a single middle element should be returned.
+// middle([1, 2, 3]) // => [2]
+// middle([1, 2, 3, 4, 5]) // => [3]
+// // For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+// middle([1, 2, 3, 4]) // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+
+// const numArr = [1, 7, 12, 3, 1000];
+// middle(numArr);
+// assertArraysEqual(numArr, ([1, 7, 12, 3, 1000]));
+// assertArraysEqual((middle(numArr)), [12]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,24 @@
 const tail = require('../tail.js');
 const assertEqual = require('../assertEqual.js');
 
-// Test Case: Check the original array 
+const assert = require('chai').assert;
+
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-let tailArray = tail(words); // no need to capture the return value since we are not checking it
-console.log("tail array = ", tailArray);
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(words.length, 4); 
-assertEqual(tailArray.length, 4); 
-assertEqual(tailArray.length, 2); 
+
+describe("#tail", () => {
+
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns " + ["Lighthouse", "Labs"] + " for words", () => {
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+
+
+});
+
+// // Test Case: Check the original array 
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(words.length, 4); 
