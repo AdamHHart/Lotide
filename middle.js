57c6f8248e594b1d7e2150baed865e60f693ea9f
Adam Hart <adamhayhart@gmail.com>
2021-03-08 13:25:20 -0800

    small change to middle.js file

--- middle.js
+++ middle.js
@@ -1,5 +1,4 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
+
 
 const middle = function(inputArray) {
   let divideArray = (inputArray.length / 2);
@@ -28,17 +27,4 @@ const middle = function(inputArray) {
 
 module.exports = middle;
 
-middle([])
-middle([1]) // => []
-middle([1, 2]) // => []
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
 
-const numArr = [1, 7, 12, 3, 1000];
-middle(numArr);
-assertArraysEqual(numArr, ([1, 7, 12, 3, 1000]));
-assertArraysEqual((middle(numArr)), [12]);
