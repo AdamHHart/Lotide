988bd04c702245568e9bb65811ae656dded4a4f9
Adam Hart <adamhayhart@gmail.com>
2021-03-08 12:47:05 -0800

    refactoring head and assert equal by exporting and requiring functions as modules

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Booyah", "Booyah");
-assertEqual(234, 243);
+
+
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual.js');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Booyah", "Booyah");
+assertEqual(234, 243);
