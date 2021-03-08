68615c0ca695acb99078ebaadc07294581f99e50
Adam Hart <adamhayhart@gmail.com>
2021-03-08 14:34:34 -0800

    EXPORTING OUR LOTIDE LIBRARY

---
+++ index.js
@@ -0,0 +1,35 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const ourMap = require('./map');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  without: without,
+  takeUntil: takeUntil,
+  map: ourMap,
+  letterPositions: letterPositions,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  assertObjectsEqual: assertObjectsEqual,
+  assertEqual: assertEqual,
+  assertArraysequal: assertArraysEqual,
+};
