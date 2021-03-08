6a2969cf755c0f6757fb259deb73cf28758d918f
Adam Hart <adamhayhart@gmail.com>
2021-03-08 13:16:15 -0800

    refactored assertArraysEqual to use modules. Created new test file.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,6 @@
 
-// const assertEqual = function(actual, expected) {
-//   if(actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
+const eqArrays = require('./eqArrays.js');
 
-const eqArrays = function(arr1, arr2) {
-  if(JSON.stringify(arr1) === JSON.stringify(arr2)) {
-    return(true);
-  } else {
-    return(false);
-  }
-} 
 
 const assertArraysEqual = function(arr1, arr2) {
   if(eqArrays(arr1, arr2)) {
@@ -24,6 +11,4 @@ const assertArraysEqual = function(arr1, arr2) {
 }
 
 
-
-assertArraysEqual([2, 6, 9], [2, 6, 9]);
-assertArraysEqual([2, 6, 9], [2, 6, 8]);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+// const eqArrays = require('../eqArrays.js');
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+
+assertArraysEqual([2, 6, 9], [2, 6, 9]);
+assertArraysEqual([2, 6, 9], [2, 6, 8]);
+
